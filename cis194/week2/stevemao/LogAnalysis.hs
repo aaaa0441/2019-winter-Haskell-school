be0cb10198e1c2767e384b2f02a578993c68f10d
Steve Mao <maochenyan@gmail.com>
2019-08-13 18:37:22 +1000

    fix drop

--- cis194/week2/stevemao/LogAnalysis.hs
+++ cis194/week2/stevemao/LogAnalysis.hs
@@ -4,9 +4,9 @@ import Log
 
 parseMessage :: String -> LogMessage
 parseMessage a = case head . words $ a of
-  "I" -> LogMessage Info (read $ (words a)!!1 :: Int) $ unwords . drop 1 . words $ a
-  "W" -> LogMessage Warning (read ((words a)!!1) :: Int) $ unwords . drop 1 . words $ a
-  "E" -> LogMessage (Error (read ((words a)!!1) :: Int)) (read ((words a)!!1) :: Int) $ unwords . drop 2 . words $ a
+  "I" -> LogMessage Info (read $ (words a)!!1 :: Int) $ unwords . drop 2 . words $ a
+  "W" -> LogMessage Warning (read ((words a)!!1) :: Int) $ unwords . drop 2 . words $ a
+  "E" -> LogMessage (Error (read ((words a)!!1) :: Int)) (read ((words a)!!1) :: Int) $ unwords . drop 3 . words $ a
   _   -> Unknown a
 
 parse :: String -> [LogMessage]
