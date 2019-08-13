a85385122ffb3b813a903f9f852738a8f87cbc27
Steve Mao <maochenyan@gmail.com>
2019-08-13 13:07:45 +1000

    week2 Exercise 1

---
+++ cis194/week2/stevemao/LogAnalysis.hs
@@ -0,0 +1,10 @@
+{-# OPTIONS_GHC -Wall #-}
+module LogAnalysis where
+import Log
+
+parseMessage :: String -> LogMessage
+parseMessage a = case head . words $ a of
+  "I" -> LogMessage Info (read ((words a)!!1) :: Int) (unwords . drop 1 . words $ a)
+  "W" -> LogMessage Warning (read ((words a)!!1) :: Int) (unwords . drop 1 . words $ a)
+  "E" -> LogMessage (Error (read ((words a)!!1) :: Int)) (read ((words a)!!1) :: Int) (unwords . drop 2 . words $ a)
+  _   -> Unknown a
